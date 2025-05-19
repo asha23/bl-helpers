b010425c2fa1cf24c8045d4f54f9313ab97c2f88
Ash Whiting <ash_whiting@hotmail.com>
2025-05-19 11:29:19 +0100

    Initial commit

---
+++ bl-helpers.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name: BL Helpers
+ * Description: A collection of optional frontend and backend tweaks for BrightLocal
+ * Version: 1.0.0
+ * Author: Ash Whiting for BrightLocal
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+// Autoload Helpers and Manager
+require_once __DIR__ . '/includes/class-blh-manager.php';
+require_once __DIR__ . '/admin/settings-page.php';
+
+// Initialize Helpers
+add_action('plugins_loaded', ['BLH_Manager', 'init']);
