d7138bc187f2cb7559a46536c0b189b0596c6798
Ash Whiting <ash_whiting@hotmail.com>
2025-05-19 11:45:06 +0100

    Update class-blh-admin-bar-padding.php

--- helpers/class-blh-admin-bar-padding.php
+++ helpers/class-blh-admin-bar-padding.php
@@ -2,18 +2,24 @@
 
 class BLH_Admin_Bar_Padding {
     public static function init() {
-        add_action('wp_head', [__CLASS__, 'add_padding_style'], 100);
+        add_action('wp_head', [__CLASS__, 'add_admin_bar_css'], 100);
     }
 
-    public static function add_padding_style() {
+    public static function add_admin_bar_css() {
         if (!is_admin_bar_showing()) {
             return;
         }
+
+        $admin_bar_height = is_admin() ? 0 : (wp_is_mobile() ? 46 : 32);
         ?>
         <style>
-            html { margin-top: 32px !important; }
+            header.fixed {
+                top: <?php echo $admin_bar_height; ?>px !important;
+            }
             @media screen and (max-width: 782px) {
-                html { margin-top: 46px !important; }
+                body {
+                    margin-top: 46px !important;
+                }
             }
         </style>
         <?php
